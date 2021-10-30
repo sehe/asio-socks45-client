09b4e13f04865b7533e469f6927769aab61e12ea
sehe <sgheeren@gmail.com>
2021-10-30 22:01:42 +0200

    publish

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # asio-socks45-client
 SOCKS4 and SOCKS5 clients for (Boost) Asio
+
+Originally authored in response to StaockOverflow questions:
+
+ - [SOCKS4 With Boost Asio](https://stackoverflow.com/a/67320567/85371)
+ - [Is there a native support for proxy connection via socks5 for boost::asio?](https://stackoverflow.com/a/69781530/85371)
+
+The interfaces are sync/async, with a shared underlying implementation.
+
+## SOCK4 Usage
+
+     tcp::socket sock{ctx};
+     tcp::endpoint
+         proxy{{}, 1080},
+         target(ip::address_v4::from_string("173.203.57.63"), 80);
+
+     std::cerr << "Using synchronous interface" << std::endl;
+     socks4::proxy_connect(sock, target,
+                           proxy); // throws system_error if failed
+                           
+Or using the async overload:
+
+     // using the async interface (still emulating synchronous by using
+     // future for brevity of this demo)
+     auto fut = socks4::async_proxy_connect(sock, target, proxy,
+                                            boost::asio::use_future);
+
+     fut.get(); // throws system_error if failed
+
+> _SOCKS4a (which also allows name resolution) is not implemented. There seems to be little support._
+
+## SOCKS5 Usage
+
+The SOCKS5 interface accepts a DNS Query as well as resolved end-point.
+
+    tcp::resolver::query target("example.com", "443");
+
+    std::future<void> conn_result = socks5::async_proxy_connect(
+        socket, target, tcp::endpoint{{}, 1080}, ba::use_future);
+
+Again, synchronous connect is also supported:
+
+    socks5::proxy_connect(socket, target, tcp::endpoint{{}, 1080});
+
+> The BIND and UDP ASSOCIATE commands are not implemented.
+
+## DEMO
+
+The answers show live demos. Here's the SOCKS5 demo using an openssh client as the SOCKS server:
+
+![SOCKS5 demo](/doc/socks5.gif?raw=true "SOCKS5 demo")
---
+++ socks4.hpp
@@ -0,0 +1,248 @@
+#include <boost/asio.hpp>
+#include <boost/endian/arithmetic.hpp>
+
+namespace socks4 { // threw in the kitchen sink for error codes
+#ifdef STANDALONE_ASIO
+    using std::error_category;
+    using std::error_code;
+    using std::error_condition;
+    using std::system_error;
+#else
+    namespace asio = boost::asio;
+    using boost::system::error_category;
+    using boost::system::error_code;
+    using boost::system::error_condition;
+    using boost::system::system_error;
+#endif
+
+    enum class result_code {
+        ok                 = 0,
+        invalid_version    = 1,
+        rejected_or_failed = 3,
+        need_identd        = 4,
+        unconirmed_userid  = 5,
+		//
+		failed = 99,
+    };
+
+    auto const& get_result_category() {
+      struct impl : error_category {
+        const char* name() const noexcept override { return "result_code"; }
+        std::string message(int ev) const override {
+          switch (static_cast<result_code>(ev)) {
+          case result_code::ok:                 return "Success";
+          case result_code::invalid_version:    return "SOCKS4 invalid reply version";
+          case result_code::rejected_or_failed: return "SOCKS4 rejected or failed";
+          case result_code::need_identd:        return "SOCKS4 unreachable (client not running identd)";
+          case result_code::unconirmed_userid:  return "SOCKS4 identd could not confirm user ID";
+          case result_code::failed:             return "SOCKS4 general unexpected failure";
+          default: return "unknown error";
+          }
+        }
+        error_condition
+        default_error_condition(int ev) const noexcept override {
+            return error_condition{ev, *this};
+        }
+        bool equivalent(int ev, error_condition const& condition)
+            const noexcept override {
+            return condition.value() == ev && &condition.category() == this;
+        }
+        bool equivalent(error_code const& error,
+                        int ev) const noexcept override {
+            return error.value() == ev && &error.category() == this;
+        }
+      } const static instance;
+      return instance;
+    }
+
+    error_code make_error_code(result_code se) {
+        return error_code{
+            static_cast<std::underlying_type<result_code>::type>(se),
+            get_result_category()};
+    }
+} // namespace socks4
+
+template <>
+struct boost::system::is_error_code_enum<socks4::result_code>
+    : std::true_type {};
+
+namespace socks4 {
+    using namespace std::placeholders;
+
+    template <typename Endpoint> struct core_t {
+        Endpoint _target;
+        Endpoint _proxy;
+
+        core_t(Endpoint target, Endpoint proxy)
+            : _target(target)
+            , _proxy(proxy) {}
+
+#pragma pack(push)
+#pragma pack(1)
+        using ipv4_octets = boost::asio::ip::address_v4::bytes_type;
+        using net_short   = boost::endian::big_uint16_t;
+
+        struct alignas(void*) Req {
+            uint8_t     version = 0x04;
+            uint8_t     cmd     = 0x01;
+            net_short   port;
+            ipv4_octets address;
+        } _request{0x04, 0x01, _target.port(),
+                   _target.address().to_v4().to_bytes()};
+
+        struct alignas(void*) Res {
+            uint8_t     reply_version;
+            uint8_t     status;
+            net_short   port;
+            ipv4_octets address;
+        } _response;
+#pragma pack(pop)
+
+        using const_buffer   = boost::asio::const_buffer;
+        using mutable_buffer = boost::asio::mutable_buffer;
+
+        auto request_buffers(char const* szUserId) const {
+            return std::array<const_buffer, 2>{
+                boost::asio::buffer(&_request, sizeof(_request)),
+                boost::asio::buffer(szUserId, strlen(szUserId) + 1)};
+        }
+
+        auto response_buffers() {
+            return boost::asio::buffer(&_response, sizeof(_response));
+        }
+
+        error_code get_result(error_code ec = {}) const {
+            if (ec)
+                return ec;
+            if (_response.reply_version != 0)
+                return result_code::invalid_version;
+
+            switch (_response.status) {
+              case 0x5a: return result_code::ok; // Request grantd
+              case 0x5B: return result_code::rejected_or_failed;
+              case 0x5C: return result_code::need_identd;
+              case 0x5D: return result_code::unconirmed_userid;
+            }
+
+            return result_code::failed;
+        }
+    };
+
+    template <typename Socket, typename Completion>
+    struct async_proxy_connect_op {
+        using Endpoint      = typename Socket::protocol_type::endpoint;
+        using executor_type = typename Socket::executor_type;
+        auto get_executor() { return _socket.get_executor(); }
+
+      private:
+        core_t<Endpoint> _core;
+        Socket&          _socket;
+        std::string      _userId;
+        Completion       _handler;
+
+      public:
+        async_proxy_connect_op(Completion handler, Socket& s, Endpoint target,
+                               Endpoint proxy, std::string user_id = {})
+            : _core(target, proxy)
+            , _socket(s)
+            , _userId(std::move(user_id))
+            , _handler(std::move(handler)) {}
+
+        using Self = std::unique_ptr<async_proxy_connect_op>;
+        void init(Self&& self) { operator()(self, INIT{}); }
+
+      private:
+        // states
+        struct INIT{};
+        struct CONNECT{};
+        struct SENT{};
+        struct ONRESPONSE{};
+
+        struct Binder {
+            Self _self;
+            template <typename... Args>
+            decltype(auto) operator()(Args&&... args) {
+                return (*_self)(_self, std::forward<Args>(args)...);
+            }
+        };
+
+        void operator()(Self& self, INIT) {
+            _socket.async_connect(_core._proxy,
+               std::bind(Binder{std::move(self)}, CONNECT{}, _1));
+        }
+
+        void operator()(Self& self, CONNECT, error_code ec) {
+            if (ec) return _handler(ec);
+            boost::asio::async_write(
+                _socket,
+                _core.request_buffers(_userId.c_str()),
+                std::bind(Binder{std::move(self)}, SENT{}, _1, _2));
+        }
+
+        void operator()(Self& self, SENT, error_code ec, size_t xfer) {
+            if (ec) return _handler(ec);
+            auto buf = _core.response_buffers();
+            boost::asio::async_read(
+                _socket, buf, boost::asio::transfer_exactly(buffer_size(buf)),
+                std::bind(Binder{std::move(self)}, ONRESPONSE{}, _1, _2));
+        }
+
+        void operator()(Self& self, ONRESPONSE, error_code ec, size_t xfer) {
+            _handler(_core.get_result(ec));
+        }
+    };
+
+    template <typename Socket,
+              typename Endpoint = typename Socket::protocol_type::endpoint>
+    error_code proxy_connect(Socket& s, Endpoint ep, Endpoint proxy,
+                             std::string const& user_id, error_code& ec) {
+        core_t<Endpoint> core(ep, proxy);
+        ec.clear();
+
+        s.connect(core._proxy, ec);
+
+        if (!ec)
+            boost::asio::write(s, core.request_buffers(user_id.c_str()),
+                               ec);
+        auto buf = core.response_buffers();
+        if (!ec)
+            boost::asio::read(s, core.response_buffers(),
+                              boost::asio::transfer_exactly(buffer_size(buf)), ec);
+
+        return ec = core.get_result(ec);
+    }
+
+    template <typename Socket,
+              typename Endpoint = typename Socket::protocol_type::endpoint>
+    void proxy_connect(Socket& s, Endpoint ep, Endpoint proxy,
+                       std::string const& user_id = "") {
+        error_code ec;
+        if (proxy_connect(s, ep, proxy, user_id, ec))
+            throw system_error(ec);
+    }
+
+    template <typename Socket, typename Token,
+              typename Endpoint = typename Socket::protocol_type::endpoint>
+    auto async_proxy_connect(Socket& s, Endpoint ep, Endpoint proxy,
+                       std::string user_id, Token&& token) {
+        using Result = asio::async_result<std::decay_t<Token>, void(error_code)>;
+        using Completion = typename Result::completion_handler_type;
+
+        Completion completion(std::forward<Token>(token));
+        Result     result(completion);
+
+        using Op = async_proxy_connect_op<Socket, Completion>;
+        // make an owning self ptr, to serve a unique async chain
+        auto self =
+            std::make_unique<Op>(completion, s, ep, proxy, std::move(user_id));
+        self->init(std::move(self));
+        return result.get();
+    }
+
+    template <typename Socket, typename Token,
+              typename Endpoint = typename Socket::protocol_type::endpoint>
+    auto async_proxy_connect(Socket& s, Endpoint ep, Endpoint proxy, Token&& token) {
+        return async_proxy_connect<Socket, Token, Endpoint>(
+            s, ep, proxy, "", std::forward<Token>(token));
+    }
+} // namespace socks4
